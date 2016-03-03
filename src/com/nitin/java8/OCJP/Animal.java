6d3ca37c27d423f6d262c7e42c59b6fe13a2cc45
Nitin Chaurasia <gs.nitin@gmail.com>
2016-03-03 00:35:00 -0800

    Lambda from OCJP book

---
+++ src/com/nitin/java8/OCJP/Animal.java
@@ -0,0 +1,31 @@
+package com.nitin.java8.OCJP;
+
+/**
+ * Created by Nitin Chaurasia on 3/3/16 at 12:18 AM.
+ */
+public class Animal {
+    private String species;
+    private boolean canHop;
+    private boolean canSwim;
+
+    public Animal(String species, boolean canHop, boolean canSwim) {
+        this.species = species;
+        this.canHop = canHop;
+        this.canSwim = canSwim;
+    }
+
+    public boolean isCanHop() {
+        return canHop;
+    }
+
+    public boolean isCanSwim() {
+        return canSwim;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "species='" + species + '\'' +
+                '}';
+    }
+}
