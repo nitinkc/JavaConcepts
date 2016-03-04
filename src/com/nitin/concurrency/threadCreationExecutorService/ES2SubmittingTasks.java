3ad29a35eec876b04266b81c8e270f012c2d90e7
Nitin Chaurasia <gs.nitin@gmail.com>
2016-03-03 21:49:11 -0800

    adding concurrency

---
+++ src/com/nitin/concurrency/threadCreationExecutorService/ES2SubmittingTasks.java
@@ -0,0 +1,10 @@
+package com.nitin.concurrency.threadCreationExecutorService;
+
+/**
+ * Created by Nitin C on 3/3/2016.
+ *
+ * Submit returns a future object that can be used to determine if the task is completed
+ */
+public class ES2SubmittingTasks {
+
+}
