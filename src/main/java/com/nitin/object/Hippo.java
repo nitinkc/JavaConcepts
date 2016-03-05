d012f6492a59efd69ff88a62b269323244053a68
Nitin Chaurasia <gs.nitin@gmail.com>
2016-03-04 23:15:26 -0800

    Object class methods learning begins

---
+++ src/main/java/com/nitin/object/Hippo.java
@@ -0,0 +1,23 @@
+package com.nitin.object;
+
+/**
+ * Created by Nitin Chaurasia on 3/4/16 at 11:01 PM.
+ */
+public class Hippo {
+    private String name;
+    private double weight;
+
+    public Hippo(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    //Explicit to String Example
+    @Override
+    public String toString() {
+        return "Hippo{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
---
+++ src/main/java/com/nitin/object/ToString.java
@@ -0,0 +1,14 @@
+package com.nitin.object;
+
+/**
+ * Created by Nitin Chaurasia on 3/4/16 at 10:59 PM.
+ * All the Classes in Java inherit from Object Class
+ */
+public class ToString {
+    public static void main(String[] args) {
+        Hippo hippo = new Hippo("Danger hippo",400.23);
+        System.out.println(hippo);
+
+
+    }
+}
