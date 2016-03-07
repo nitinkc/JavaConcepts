214890c887e4c0be01808cd214a2ee942346fbe7
Nitin Chaurasia <gs.nitin@gmail.com>
2016-03-06 23:57:55 -0800

    Class design begins

---
+++ src/com/nitin/designPatternsNPrinciples/inheritance/Animal.java
@@ -0,0 +1,29 @@
+package com.nitin.designPatternsNPrinciples.inheritance;
+
+/**
+ * Created by Nitin Chaurasia on 3/6/16 at 11:37 PM.
+ */
+public class Animal {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Animal(int age){
+
+    }
+
+
+}
+
+class Lion extends Animal{
+
+    public Lion(int age) {
+        super(age);
+    }
+}
---
+++ src/com/nitin/designPatternsNPrinciples/interfaceImpl/Primate.java
@@ -0,0 +1,7 @@
+package com.nitin.designPatternsNPrinciples.interfaceImpl;
+
+/**
+ * Created by Nitin Chaurasia on 3/6/16 at 11:57 PM.
+ */
+public class Primate {
+}
