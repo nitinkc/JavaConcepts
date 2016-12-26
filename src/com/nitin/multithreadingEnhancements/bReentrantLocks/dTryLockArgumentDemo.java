169ceb7be9ce7d7e59dc7fd1b6f5f6ebffaded52
Nitin Chaurasia <gs.nitin@gmail.com>
2016-12-26 05:17:13 -0500

    Thread Pool and Executor FW

--- src/com/nitin/multithreadingEnhancements/bReentrantLocks/dTryLockArgumentDemo.java
+++ src/com/nitin/multithreadingEnhancements/bReentrantLocks/dTryLockArgumentDemo.java
@@ -2,6 +2,8 @@ package com.nitin.multithreadingEnhancements.bReentrantLocks;
 
 /**
  * Created by nitin.chaurasia on 12/26/2016.
+ *
+ * Second thread will continue to pull if the Lock is available or not
  */
 public class dTryLockArgumentDemo {
     public static void main(String[] args) {
---
+++ src/com/nitin/multithreadingEnhancements/dThreadLocal/dInheritableThreadLocal.java
@@ -0,0 +1,31 @@
+package com.nitin.multithreadingEnhancements.dThreadLocal;
+
+/**
+ * Created by nitin.chaurasia on 12/26/2016.
+ */
+public class dInheritableThreadLocal {
+    public static void main(String[] args) {
+        ParentThread1 pt = new ParentThread1();
+        pt.start();
+    }
+
+
+}
+class ParentThread1 extends Thread{
+    public static InheritableThreadLocal tl = new InheritableThreadLocal();
+    public void run(){
+        tl.set("Parent Thread Local");
+        System.out.println("Parent Thread Local Value = " + tl.get());
+
+        //Creating the Child Thread from Parent
+        ChildThread ct = new ChildThread();
+        ct.run();
+    }
+}
+
+class ChildThread1 extends Thread {
+    public void run() {
+        System.out.println("Child Thread Local Value = " +
+                ParentThread.tl.get());//Null - should be Parent Thread Local : REDO LAST VIDEO TOWARDS END
+    }
+}
