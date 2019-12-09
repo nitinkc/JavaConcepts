05489b435a8b5583c6c9c956308c03fc8ad6950b
Nitin Chaurasia <gs.nitin@gmail.com>
2019-12-09 18:46:28 -0500

    Update FunctionUsage.java

--- src/main/java/com/nitin/a16functionalInterface/PredefinedFunctionalInterfaces/functionUsage/FunctionUsage.java
+++ src/main/java/com/nitin/a16functionalInterface/PredefinedFunctionalInterfaces/functionUsage/FunctionUsage.java
@@ -13,12 +13,13 @@ import java.util.function.Predicate;
  */
 public class FunctionUsage {
     public static final int DEFAULT_VALUE = 9999;
+
     public static void main(String[] args) {
 
-        List<String> list = Arrays.asList("1","2","3", "n", "",null);
+        List<String> list = Arrays.asList("1", "2", "3", "n", "", null);
 
         // Function to convert Strings to Int, put 9999 as default value for other cases
-        Function<String, Integer> function = x -> NumberUtils.toInt(x,DEFAULT_VALUE);
+        Function<String, Integer> function = x -> NumberUtils.toInt(x, DEFAULT_VALUE);
 
         Predicate<Integer> predicate = (Integer x) -> (x == DEFAULT_VALUE);
         //MAP is used to apply a function
@@ -32,6 +33,5 @@ public class FunctionUsage {
                 .map(function)
                 .filter(predicate.negate())
                 .forEach(System.out::println);
-
     }
 }
