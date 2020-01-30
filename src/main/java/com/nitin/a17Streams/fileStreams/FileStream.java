eee4e2b194e5d22725ffa3cd3bdf1bc12982cb0c
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-30 00:57:44 -0500

    file streams read

--- src/main/java/com/nitin/a17Streams/fileStreams/FileStream.java
+++ src/main/java/com/nitin/a17Streams/fileStreams/FileStream.java
@@ -1,15 +1,15 @@
 package com.nitin.a17Streams.fileStreams;
 
-import java.io.File;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,18 +19,40 @@ import java.util.stream.Stream;
 
 public class FileStream {
     public static void main(String args[]) throws URISyntaxException, IOException {
-        String fileName = "src/main/resources/cancer.csv";
+        String fileName = "src/main/resources/temp.csv";
         Path path = Paths.get(fileName);
+        List<Cancer> list = null;
 
         //read file into stream, try-with-resources
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-            stream
+            list = stream
                     .map(str -> str.split(","))
-                    .collect(Collectors.groupingBy()
-                    .forEach((str, lng) -> System.out.println(str[0] + " : " +lng));
-                  //.filter(str -> str.equalsIgnoreCase("Alabama"))
+                    .skip(1)
+
+                    .map(strArr -> {
+                        String temp[] = strArr;
+                        Cancer c;
+                        if(null != temp[0] && null != temp[1] && null != temp[2] &&
+                        null != temp[3] && null != temp[4] && null != temp[5] && null != temp[6]){
+                            c = new Cancer(
+                                    StringUtils.defaultString(temp[0],null),
+                                    StringUtils.defaultString(temp[1],null),
+                                    NumberUtils.toInt(temp[2], -1),
+                                    StringUtils.defaultString(temp[3],null),
+                                    StringUtils.defaultString(temp[4],null),
+                                    NumberUtils.toFloat(temp[5],-1),
+                                    NumberUtils.toInt(temp[6],-1),
+                                    NumberUtils.toFloat(temp[7],-1)
+                            );
+                        }else{
+                            c = new Cancer();
+                        }
+                        return c; })
+                    .collect(Collectors.toCollection(ArrayList::new));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //list.stream().forEach(System.out::println);
     }
 }
