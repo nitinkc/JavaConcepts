1312bdba3042bd52661bdd6ecf8cfc12a8053d1d
Nitin Chaurasia <gs.nitin@gmail.com>
2020-02-15 22:03:03 -0500

    Update Words.java

--- Java8/src/main/java/com/nitin/LexicalScopingClosures/Words.java
+++ Java8/src/main/java/com/nitin/LexicalScopingClosures/Words.java
@@ -1,38 +1,18 @@
 package com.nitin.LexicalScopingClosures;
-
 import org.json.simple.JSONObject;
-
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.stream.Collectors;
 
 /**
  * Created by nitin on Thursday, February/13/2020 at 1:53 AM
  */
 public class Words {
 
-
-    public static <JSONException> JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            JSONObject json = new JSONObject(jsonText);
-            return json;
-        } finally {
-            is.close();
-        }
-    }
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
         JSONObject json = null;
-        try {
-            json = readJsonFromUrl("https://graph.facebook.com/19292868552");
-        } catch (RuntimeException o) {
-            o.printStackTrace();
-        }
+        json = readJsonFromUrl("https://graph.facebook.com/19292868552");
+
         System.out.println(json.toString());
         System.out.println(json.get("id"));
         /*try {
@@ -42,13 +22,21 @@ public class Words {
                     new BufferedReader(new InputStreamReader(url.openStream()));
 
             System.out.println(reader.readLine());
-
-
         } catch(Exception ex) {
             throw new RuntimeException(ex);
         }*/
+    }
 
-
+    public static <JSONException> JSONObject readJsonFromUrl(String url) throws IOException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
     }
 
     private static String readAll(Reader rd) throws IOException {
