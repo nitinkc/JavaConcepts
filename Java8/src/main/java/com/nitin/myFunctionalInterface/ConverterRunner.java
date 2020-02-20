43f88c05deb6697c0d45fe32deb60567d39082d8
Nitin Chaurasia <gs.nitin@gmail.com>
2020-02-20 00:50:36 -0500

    Update ConverterRunner.java

--- Java8/src/main/java/com/nitin/myFunctionalInterface/ConverterRunner.java
+++ Java8/src/main/java/com/nitin/myFunctionalInterface/ConverterRunner.java
@@ -6,8 +6,16 @@ package com.nitin.myFunctionalInterface;
 public class ConverterRunner {
     public static void main(String[] args) {
 
-        final Convertor<Integer, Float> Celcius2Fheight = x -> x * (9f / 5) + 32;
+        //final Convertor<Integer, Float> Celcius2Fheight = x -> x * (9f / 5) + 32;
 
+        // Using Anonymous Class
+        final Convertor<Integer, Float> celcius2Fheight = new Convertor<Integer, Float>() {
+            @Override
+            public Float convert(Integer x) {
+                return x * (9f / 5) + 32;
+            }
+        };
 
+        celcius2Fheight.convert(23);
     }
 }
