5f184b35be5c0f67bd73a451aa7a5ea7318d3133
Nitin Chaurasia <gs.nitin@gmail.com>
2020-04-23 23:06:57 -0400

    Create GetNextMonthsNYearDates.java

---
+++ Java8/src/main/java/com/nitin/a19CalanderNCurrency/calanderDateTime/GetNextMonthsNYearDates.java
@@ -0,0 +1,36 @@
+package com.nitin.a19CalanderNCurrency.calanderDateTime;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by nichaurasia on Thursday, April/23/2020 at 10:57 AM
+ */
+
+public class GetNextMonthsNYearDates {
+    public static void main(String[] args) {
+
+        Calendar calendar = Calendar.getInstance();
+        Date currentDate = calendar.getTime();
+        Timestamp authTimestamp = new Timestamp(currentDate.getTime());
+
+        System.out.println(authTimestamp);
+
+        /*
+        If Current Date Dt: 04/17/2020
+        POI -> 05/17/2020 to 05/16/2021.
+        */
+        //Add one month to the current Date
+        calendar.add(Calendar.MONTH, 1);
+        Date poiBeginDate = calendar.getTime();
+        //Add 12 months from one month of the current Date
+        calendar.add(Calendar.MONTH, 12);
+        //
+        calendar.add(Calendar.DATE,-1);
+        Date poiEndDate = calendar.getTime();
+
+        Timestamp start = new Timestamp(poiBeginDate.getTime());
+        Timestamp end = new Timestamp(poiEndDate.getTime());
+    }
+}
