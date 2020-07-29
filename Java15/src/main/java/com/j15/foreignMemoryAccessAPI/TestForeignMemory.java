e45f6600d1a55b51e81ba4a2abe13997274f8d1f
Nitin Chaurasia <gs.nitin@gmail.com>
2020-07-29 01:59:19 -0400

    Create TestForeignMemory.java

---
+++ Java15/src/main/java/com/j15/foreignMemoryAccessAPI/TestForeignMemory.java
@@ -0,0 +1,22 @@
+package com.j15.foreignMemoryAccessAPI;
+
+import jdk.incubator.foreign.MemoryAddress;
+import jdk.incubator.foreign.MemoryHandles;
+import jdk.incubator.foreign.MemorySegment;
+
+import java.lang.invoke.VarHandle;
+import java.nio.ByteOrder;
+
+public class TestForeignMemory {
+    public static void main(String[] args) {
+        VarHandle intHandle = MemoryHandles.varHandle(int.class,
+                ByteOrder.nativeOrder());
+
+        try (MemorySegment segment = MemorySegment.allocateNative(100)) {
+            MemoryAddress base = segment.baseAddress();
+            for (int i = 0; i < 25; i++) {
+                intHandle.set(base.addOffset(i * 4), i);
+            }
+        }
+    }
+}
