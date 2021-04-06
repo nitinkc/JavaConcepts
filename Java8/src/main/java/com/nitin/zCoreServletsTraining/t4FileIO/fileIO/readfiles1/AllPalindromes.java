27176ba89ebfab3524b603d16e09d7914fa18081
Nitin Chaurasia <nitin.chaurasia@davita.com>
2021-04-06 02:55:56 -0500

    partition

--- Java8/src/main/java/com/nitin/zCoreServletsTraining/t4FileIO/fileIO/readfiles1/AllPalindromes.java
+++ Java8/src/main/java/com/nitin/zCoreServletsTraining/t4FileIO/fileIO/readfiles1/AllPalindromes.java
@@ -9,7 +9,7 @@ import com.nitin.zCoreServletsTraining.t4FileIO.fileIO.strings.*;
 
 public class AllPalindromes {
   public static void main(String[] args) throws Exception {
-    String inputFile = "Java8/src/main/java/com/nitin/zCoreServletsTraining/t4FileIO/fileIO/enable1-word-list.txt";
+    String inputFile = "Java8/src/main/java/com/nitin/zCoreServletsTraining/t4FileIO/fileIO/readfiles1/enable1-word-list.txt";
 
     Files.lines(Paths.get(inputFile))
          .filter(StringUtils::isPalindrome)
---
+++ JavaLatest/src/main/java/com/google/FileWrite.java
@@ -0,0 +1,25 @@
+package com.google;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+
+public class FileWrite {
+
+    public static void doSomethingParallely(String singleString) {
+        Charset characterSet = Charset.defaultCharset();
+        Path path = Paths.get("JavaLatest/src/main/java/com/google/output.txt");
+        try {
+            //System.out.println("Writing into file the word :: " + singleString);
+            //Files.write(path, Collections.singleton(singleString), characterSet, StandardOpenOption.APPEND);
+            Files.write(path, Collections.singleton(singleString), characterSet);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
