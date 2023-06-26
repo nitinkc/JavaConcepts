97f2ffb2f1cf34f58361b9b1bebf4055e07f5fa8
Nitin Chaurasia <gs.nitin@gmail.com>
2023-06-25 21:43:24 -0600

    updates

---
+++ src/main/java/sandbox/ZPatternExampleDito.java
@@ -0,0 +1,29 @@
+package sandbox;
+
+import com.itextpdf.dito.binding.DataContext;
+import com.itextpdf.dito.binding.DefaultDataContext;
+import com.itextpdf.dito.builder.DitoDocumentBuilder;
+import com.itextpdf.dito.builder.model.document.DocumentNode;
+import com.itextpdf.dito.data.ValueType;
+
+import java.io.FileOutputStream;
+
+public class ZPatternExampleDito {
+    public static void main(String[] args) throws Exception {
+        // Create a data context
+        DataContext dataContext = new DefaultDataContext();
+        dataContext.setValue("value1", "A", ValueType.STRING);
+        dataContext.setValue("value2", "B", ValueType.STRING);
+        dataContext.setValue("value3", "C", ValueType.STRING);
+
+        // Load the template
+        DocumentNode documentNode = DitoDocumentBuilder.parseTemplate("z_pattern.dito");
+
+        // Bind the data context to the template
+        documentNode.setDataContext(dataContext);
+
+        // Generate the output PDF
+        DitoDocumentBuilder.buildDocument(documentNode, new FileOutputStream("z_pattern_output.pdf"));
+    }
+}
+
